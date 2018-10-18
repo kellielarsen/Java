c0fec5b4e0f9feb37008fd6542d282dd51d21715
Kellie Larsen <kdlarsen@mavs.coloradomesa.edu>
2018-10-18 11:50:34 -0600

    update

--- Threads_SynchronizationProject/src/main/java/com/github/kellielarsen/threads_synchronizationproject/Main.java
+++ Threads_SynchronizationProject/src/main/java/com/github/kellielarsen/threads_synchronizationproject/Main.java
@@ -5,10 +5,10 @@ public class Main {
     public static void main(String args[]) {
         Tickets ticketCount = new Tickets();
         Thread p1 = new Thread(new TicketThread(ticketCount, "pass1", 2));
-        Thread p2 = new Thread(new TicketThread(ticketCount, "pass2", 1));
+        Thread p2 = new Thread(new TicketThread(ticketCount, "pass2", -1));
         Thread p3 = new Thread(new TicketThread(ticketCount, "pass3", 3));
-        Thread p4 = new Thread(new TicketThread(ticketCount, "pass4", 2));
-        Thread p5 = new Thread(new TicketThread(ticketCount, "pass5", 4));
+        Thread p4 = new Thread(new TicketThread(ticketCount, "pass4", 0));
+        Thread p5 = new Thread(new TicketThread(ticketCount, "pass5", 6));
         p1.start();
         p2.start();
         p3.start();
--- Threads_SynchronizationProject/src/main/java/com/github/kellielarsen/threads_synchronizationproject/Tickets.java
+++ Threads_SynchronizationProject/src/main/java/com/github/kellielarsen/threads_synchronizationproject/Tickets.java
@@ -9,7 +9,11 @@ public class Tickets {
             System.out.println(name + ": " + numSeats + " ticket(s) booked successfully.");
             availableSeats -= numSeats;
         }
+        else if (numSeats == 0)
+            System.out.println(name + ": No tickets requested.");
+        else if (numSeats < 0)
+            System.out.println(name + ": Error - invalid number of seats requested.");
         else
-            System.out.println(name + ": seats are not available.");
+            System.out.println(name + ": " + numSeats + " seats are not available.");
     }
 }
