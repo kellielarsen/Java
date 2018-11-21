373c8036ee5c0892849ca1881702b7dcde1e0e09
Kellie Larsen <kdlarsen@mavs.coloradomesa.edu>
2018-11-20 18:39:04 -0700

    proxy client tests passed

--- FinalProject/src/test/java/com/github/kellielarsen/finalproject/ProxyClientTest.java
+++ FinalProject/src/test/java/com/github/kellielarsen/finalproject/ProxyClientTest.java
@@ -1,5 +1,7 @@
 package com.github.kellielarsen.finalproject;
 
+import java.io.IOException;
+import java.net.Socket;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -29,54 +31,34 @@ public class ProxyClientTest {
     public void tearDown() {
     }
 
-    /**
-     * Test of getUsername method, of class ProxyClient.
-     */
+    /* Test of getUsername method, of class ProxyClient. */
     @org.junit.Test
-    public void testGetUsername() {
+    public void testGetUsername() throws IOException {
         System.out.println("getUsername");
-        ProxyClient instance = null;
-        String expResult = "";
+        Server server = new Server(5000);
+        server.start();
+        Socket socket = new Socket("127.0.0.1", 5000);
+        ProxyClient instance = new ProxyClient(server, socket);
+        instance.username = "Test";
+        String expResult = "Test";
         String result = instance.getUsername();
         assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
 
-    /**
-     * Test of handleLogin method, of class ProxyClient.
-     */
-    @org.junit.Test
-    public void testHandleLogin() throws Exception {
-        System.out.println("handleLogin");
-        ProxyClient instance = null;
-        instance.handleLogin();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    /* No test needed for handleLogin method, of class ProxyClient. */
 
-    /**
-     * Test of handleChat method, of class ProxyClient.
-     */
-    @org.junit.Test
-    public void testHandleChat() {
-        System.out.println("handleChat");
-        ProxyClient instance = null;
-        instance.handleChat();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+    /* No test needed for handleChat method, of class ProxyClient. */
 
-    /**
-     * Test of run method, of class ProxyClient.
-     */
+    /* Test of run method, of class ProxyClient. */
     @org.junit.Test
-    public void testRun() {
+    public void testRun() throws IOException {
         System.out.println("run");
-        ProxyClient instance = null;
-        instance.run();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
+        Server server = new Server(5000);
+        server.start();
+        Socket socket = new Socket("127.0.0.1", 5000);
+        ProxyClient instance = new ProxyClient(server, socket);
+        instance.start();
+        assertTrue(instance.isAlive());
     }
     
 }
